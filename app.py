1b4e6edd4a35d2be75aaf9f272c57701efe584ba
james200100 <93053780+james200100@users.noreply.github.com>
2023-10-07 07:37:32 +0000

    day_2

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
-from flask import Flask
+from flask import Flask, jsonify,render_template
 app=Flask(__name__)
 
+JOBS=[
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':'Bengaluru, India',
+    'salary': 'Rs. 10,10,000'
+    
+  },
+  {
+    'id':2,
+    'title':'Data Scientist',
+    'location':'Delhi, India',
+    'salary': 'Rs. 15,10,000'
+  }
+  ,{
+    'id':3,
+    'title':'Frontend Engineer',
+    'location':'Remote',
+    'salary': 'Rs. 12,10,000'
+  }
+  ,{
+    'id':4,
+    'title':'Backend Engineer',
+    'location':'Mumbai, India',
+    'salary': 'Rs. 20,10,000'
+  }
+]
 @app.route("/")
 def hello_world():
-  return "Hello, Raj Tyagi "
+  return render_template("home.html",jobs=JOBS)
+  @app.route("/jobs")
+  def list_jobs():
+    return jsonify(JOBS)
 
 print(__name__)
 
